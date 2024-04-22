2242a1049418a1d90a86da2b6764fee23ed7def1
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-04-22 14:18:38 +0100

    Added Update, Delete

--- server.js
+++ server.js
@@ -12,11 +12,26 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from Node API Updated");
 });
+
 // View the products
 app.get("/api/blogs", async (req, res) => {
   try {
     const blogs = await Blog.find({});
+
     res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get with id
+app.get("/api/blog/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const blog = await Blog.findById(id);
+
+    res.status(200).json(blog);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -27,11 +42,47 @@ app.post("/api/blog", async (req, res) => {
   try {
     // This saves the blog we enter to the database
     const blog = await Blog.create(req.body);
+
     res.status(200).json(blog);
   } catch (error) {
     //This shows the error message on the webpage
     res.status(500).json({ message: error.message });
+
     console.log(error);
+  }
+});
+
+// Update prodduct
+app.put("/api/blog/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const blog = await Blog.findByIdAndUpdate(id, req.body);
+    // Check to ensure there is an item with that id
+    if (!blog) {
+      return res.status(404).json({ message: "Not Found" });
+    }
+    // Updates the item and returns with a response
+    const updatedBlog = await Blog.findById(id);
+    res.status(200).json(updatedBlog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// Delete a blog
+app.delete("/api/blog/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const blog = await Blog.findByIdAndDelete(id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
@@ -44,6 +95,7 @@ mongoose
   // runs this code after a successful connection
   .then(() => {
     console.log("Connected!");
+
     // Listens to check that the app is on the right port
     app.listen(3000, function () {
       console.log("Server running on port 3000!");
